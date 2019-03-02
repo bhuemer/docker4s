7a369da0782782031cb3696f03291f301ae48a54
Bernhard Huemer <bernhard.huemer@gmail.com>
2019-03-02 19:29:17 +0000

    - Working on unix domain socket channels using Netty

---
+++ docker-client/src/main/scala/org/docker4s/DockerClient.scala
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2019 Bernhard Huemer
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.docker4s
+
+/**
+  * Allows you to communicate with a docker daemon.
+  */
+trait DockerClient {
+
+
+
+}
+
+object DockerClient {
+
+  /**
+    * Returns a [[DockerClient]] configured from environment variables.
+    *
+    * The environment variables used are the same as those used by the Docker command-line client. They are:
+    *  - '''DOCKER_HOST''' - the URL to the Docker host
+    *  - '''DOCKER_TLS_VERIFY''' - verify the host against a CA certificate
+    *  - '''DOCKER_CERT_PATH''' - path to a directory containing TLS certificates to use when connecting
+    */
+  def fromEnvironment: DockerClient = ???
+
+}

97977a7a73098ce899b3ad21056bf29fc0a424df
Bernhard Huemer <bernhard.huemer@gmail.com>
2019-04-06 16:01:42 +0100

    - Adding a test for version information

---
+++ docker-client/src/test/scala/org/docker4s/api/SystemSpec.scala
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2019 Bernhard Huemer
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.docker4s.api
+
+import com.typesafe.scalalogging.LazyLogging
+import org.junit.runner.RunWith
+import org.scalatest.Matchers
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class SystemSpec extends ClientSpec with Matchers with LazyLogging {
+
+  "The docker client" should "be able to detect version information" given { client =>
+    for {
+      version <- client.system.version
+    } yield {
+      logger.info(s"Version: $version")
+    }
+  }
+
+}
